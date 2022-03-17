5e221e3e77856715bef72554c6ae9599a9fe2bbf
Sebastian Erfurth <se.erfurth@gmail.com>
2022-03-17 20:03:12 +0100

    Fix for 403 and 404 errors

--- controller/horizon/horizon.sh
+++ controller/horizon/horizon.sh
@@ -7,7 +7,10 @@ dnf -y install openstack-dashboard
 cp local_settings /etc/openstack-dashboard/local_settings
 sed -i "s/HORIZON_PASS/${HORIZON_PASS}/g" /etc/openstack-dashboard/local_settings
 sed -i "s/ALLOWED_HOSTS_VALUE/${ALLOWED_HOSTS_VALUE}/g" /etc/openstack-dashboard/local_settings
-echo "WSGIApplicationGroup %{GLOBAL}" >> /etc/httpd/conf.d/openstack-dashboard.conf
+cp openstack-dashboard.conf /etc/httpd/conf.d/openstack-dashboard.conf
+
+firewall-cmd --add-service=http --permanent
+firewall-cmd --reload
 
 systemctl enable httpd.service
 systemctl restart httpd.service memcached.service
