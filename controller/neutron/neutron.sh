8305c90bd7a8d901b91ba1c96cabb04de3528668
Sebastian Erfurth <se.erfurth@gmail.com>
2022-04-01 22:12:57 +0200

    Changed controller ip as variable in linuxbridge config and turned os_neutron_dac_override on

--- controller/neutron/neutron.sh
+++ controller/neutron/neutron.sh
@@ -23,15 +23,16 @@ sed -i "s/NOVA_PASS/${NOVA_PASS}/g" /etc/neutron/neutron.conf
 cp ml2_conf.ini /etc/neutron/plugins/ml2/ml2_conf.ini
 cp linuxbridge_agent.ini /etc/neutron/plugins/ml2/linuxbridge_agent.ini
 sed -i "s/PROVIDER_INTERFACE_NAME/${PROVIDER_INTERFACE_NAME}/g" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
+sed -i "s/CONTROLLER_IP/${CONTROLLER_IP}/g" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
 cp l3_agent.ini /etc/neutron/l3_agent.ini
 cp dhcp_agent.ini /etc/neutron/dhcp_agent.ini
 echo "net.bridge.bridge-nf-call-iptables = 1
 net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.conf 
 cp metadata_agent.ini /etc/neutron/metadata_agent.ini
 sed -i "s/METADATA_SECRET/${METADATA_SECRET}/g" /etc/neutron/metadata_agent.ini
-
 ln -s /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugin.ini
 su -s /bin/sh -c "neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head" neutron
+setsebool os_neutron_dac_override on
 
 systemctl restart openstack-nova-api.service
 systemctl enable neutron-server.service neutron-linuxbridge-agent.service neutron-dhcp-agent.service neutron-metadata-agent.service
